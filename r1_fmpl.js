cae221f4f5f557b29250bc2feedd121d7724d1a9
Ashley <yy346@cornell.edu>
2022-10-13 21:40:30 -0400

    adding debugging statements

--- r1_fmpl.js
+++ r1_fmpl.js
@@ -16,7 +16,7 @@ Qualtrics.SurveyEngine.addOnReady(function()
     const toFloat = (field, decimal) => {
         return +parseFloat(field).toFixed(decimal);
     }
-    
+
     const switchpoint = parseInt("${e://Field/switchpoint_main_r1}");
     let price_init = parseInt("${e://Field/price_init}");
     let price_incr = parseInt("${e://Field/price_incr}");
@@ -151,36 +151,23 @@ Qualtrics.SurveyEngine.addOnReady(function()
     }
 
     function displayLabels_v2(QID, init_eff, incr_eff, init_trad, incr_trad, disc_rate) {
-        let eff_caps = "${e://Field/efficient_allcaps}";
-        let trad_caps = "${e://Field/traditional_allcaps}";
-        let num = parseInt("${e://Field/display_order}");
-        const rows = document.getElementsByClassName("ChoiceRow");
+        let eff_value = (iseffLeft()) ? 1 : 2;
+        const rows = question.getElementsByClassName("ChoiceRow");
         //console.log(num);
         for (let i = 0; i < rows.length; i++) {
-            const ida = QID+"-"+(i+basenum).toString()+"-1-label";
-            const idb = QID+"-"+(i+basenum).toString()+"-2-label";
+            const row = rows[i];
+            const inputs = row.getElementsByTagName("input");
+            const input_eff = getInputByValue(inputs, eff_value);
+            const input_trad = getInputByValue(inputs, 3-eff_value);
+            const label_eff = input_eff.labels[0];
+            const label_trad = input_trad.labels[0];
+
             const eff_disc = (init_eff + i * incr_eff).toFixed(2).replace(/\.00$/, '');
             const eff_original = (eff_disc / disc_rate).toFixed(2).replace(/\.00$/, '');
             let trad = (init_trad + i * incr_trad).toFixed(2).replace(/\.00$/, '');
-            if (num === 0) {
-                if (i === 0) {
-                    document.getElementById(ida).innerHTML="<u>Choice A</u>:&nbsp;<br /><strong>" + eff_caps + "</strong><br /><img alt='eff' height=\"77\" src=\"https://cornell.ca1.qualtrics.com/CP/Graphic.php?IM=IM_3eM0Z9xS5Nz4GXk\" style=\"width: 175px; height: 77px;\" width=\"175\" /><br /><br /><strong><s>$"+(eff_original)+"</s><span style=\"color:red\"> $" + (eff_disc)+"</span></strong>";
-                    document.getElementById(idb).innerHTML="<u>Choice B</u>:&nbsp;<br /><strong>" + trad_caps + "</strong><br /><img alt='trad' height=\"80\" src=\"https://cornell.ca1.qualtrics.com/CP/Graphic.php?IM=IM_bOy1igCnrZLIX4y\" style=\"width: 150px; height: 80px;\" width=\"150\" /><br /><br /><strong>$"+(trad)+"</strong>";
-                }
-                else {
-                    document.getElementById(ida).innerHTML="<strong><s>$"+eff_original+"</s><span style=\"color:red\"> $" + eff_disc +"</span></strong>";
-                    document.getElementById(idb).innerHTML="<strong>$"+trad+"</strong>";
-                }
-            } else {
-                if (i === 0) {
-                    document.getElementById(idb).innerHTML="<u>Choice B</u>:&nbsp;<br /><strong>" + eff_caps + "</strong><br /><img alt='eff' height=\"77\" src=\"https://cornell.ca1.qualtrics.com/CP/Graphic.php?IM=IM_3eM0Z9xS5Nz4GXk\" style=\"width: 175px; height: 77px;\" width=\"175\" /><br /><br /><strong><s>$"+(eff_original)+"</s><span style=\"color:red\"> $" + (eff_disc)+"</span></strong>";
-                    document.getElementById(ida).innerHTML="<u>Choice A</u>:&nbsp;<br /><strong>" + trad_caps + "</strong><br /><img alt='trad' height=\"80\" src=\"https://cornell.ca1.qualtrics.com/CP/Graphic.php?IM=IM_bOy1igCnrZLIX4y\" style=\"width: 150px; height: 80px;\" width=\"150\" /><br /><br /><strong>$"+(trad)+"</strong>";
-                }
-                else {
-                    document.getElementById(idb).innerHTML="<strong><s>$"+eff_original+"</s><span style=\"color:red\"> $" + eff_disc +"</span></strong>";
-                    document.getElementById(ida).innerHTML="<strong>$"+trad+"</strong>";
-                }
-            }
+
+            label_eff.innerHTML = "<strong><s>$"+eff_original+"</s><span style=\"color:red\"> $" + eff_disc +"</span></strong>";
+            label_trad.innerHTML = "<strong>$"+trad+"</strong>";
         }
     }
 
@@ -195,6 +182,10 @@ Qualtrics.SurveyEngine.addOnReady(function()
             "init_e": null,
             "init_t": null
         };
+
+        console.log("lower bound eff is ", parseFloat("${e://Field/lower_bound_eff_main_r1}"));
+        console.log("reduc rate is ", parseFloat("${e://Field/reduc_rate}"));
+        console.log("incr eff swi is ", parseFloat("${e://Field/fmpl_eff_incr_swi}"));
 
         //@TODO: ** change the eff fmpl init price variable when there's a switchpoint and when choice_a is eff **
         let inite_sw_ea = parseFloat("$e{ ( e://Field/lower_bound_eff_main_r1 + e://Field/fmpl_eff_incr_swi / e://Field/reduc_rate ) }");
